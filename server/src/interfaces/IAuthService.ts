0b812d2c45cfaaaa5dafaea10b2e296d35dd3f8e
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-08 21:30:28 +0300

    added interface for AuthService

---
+++ server/src/interfaces/IAuthService.ts
@@ -0,0 +1,9 @@
+import {Request, Response} from "express";
+
+export interface IAuthService {
+	registration(req: Request, res: Response): Promise<void>
+	login(req: Request, res: Response): Promise<void>
+	logout(req: Request, res: Response): Promise<void>
+	activateAccount(req: Request, res: Response): Promise<void>
+	refresh(req: Request, res: Response): Promise<void>
+}
