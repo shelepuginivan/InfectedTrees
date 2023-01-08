4db49b0b183947b6c4eb9ffa24ce4effe9e6ae5b
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-08 20:11:23 +0300

    renamed interface to IAuthController

--- server/src/interfaces/IAuthController.ts
+++ server/src/interfaces/IAuthController.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
 
-export interface IUserController {
+export interface IAuthController {
 	registration(req: Request, res: Response): Promise<void>
 	login(req: Request, res: Response): Promise<void>
 	logout(req: Request, res: Response): Promise<void>
