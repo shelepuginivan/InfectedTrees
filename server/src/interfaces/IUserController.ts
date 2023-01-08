889776e2185530e87686770d1a9ab80a56217536
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-08 20:09:58 +0300

    added account methods

--- server/src/interfaces/IUserController.ts
+++ server/src/interfaces/IUserController.ts
@@ -4,4 +4,6 @@ export interface IUserController {
 	registration(req: Request, res: Response): Promise<void>
 	login(req: Request, res: Response): Promise<void>
 	logout(req: Request, res: Response): Promise<void>
+	activateAccount(req: Request, res: Response): Promise<void>
+	refresh(req: Request, res: Response): Promise<void>
 }
