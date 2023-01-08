a9e6253590bd1711f31e4621f5903d812dc1d10e
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-08 20:52:54 +0300

    added router for authorization

---
+++ server/src/routers/authRouter.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+import AuthController from "../controllers/AuthController";
+
+const authRouter: Router = Router()
+
+authRouter.post('/registration', AuthController.registration)
+authRouter.post('/login', AuthController.login)
+authRouter.post('/logout', AuthController.logout)
+authRouter.get('/activate/:link', AuthController.activateAccount)
+authRouter.get('/refresh', AuthController.refresh)
+
+export default authRouter
