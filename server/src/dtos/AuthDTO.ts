4c52853ac62de88152454c0292552fdd1b987305
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-09 19:53:15 +0300

    implemented class Auth DTO

---
+++ server/src/dtos/AuthDTO.ts
@@ -0,0 +1,11 @@
+class AuthDTO implements IAuthDTO {
+	accessToken: string;
+	user: IUserDTO;
+
+	constructor(data: any) {
+		this.accessToken = data.accessToken
+		this.user = data.user
+	}
+}
+
+export default AuthDTO
