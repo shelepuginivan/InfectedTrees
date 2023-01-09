0859230018f9d5be152bf6553e07e60c85dad18a
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-09 15:03:59 +0300

    added id field from MongoDB to UserDTO

--- server/src/dtos/UserDTO.ts
+++ server/src/dtos/UserDTO.ts
@@ -1,10 +1,12 @@
 class UserDTO implements IUserDTO {
+	id: string
 	firstname: string;
 	lastname: string;
 	email: string;
 	isActivated: boolean
 
 	constructor(userDocument: any) {
+		this.id = userDocument._id
 		this.firstname = userDocument.firstname
 		this.lastname = userDocument.lastname
 		this.email = userDocument.email
