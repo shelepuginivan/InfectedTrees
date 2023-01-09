4c86cbbc1477eefdb5b187a1b7712b6b7b4e9f59
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-09 21:13:33 +0300

    added GET / request handler

--- server/src/routers/infectedTreeRouter.ts
+++ server/src/routers/infectedTreeRouter.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
+import InfectedTreeController from "../controllers/InfectedTreeController";
 
 const infectedTreeRouter: Router = Router()
 
-infectedTreeRouter.get('/')
-infectedTreeRouter.post('/')
-infectedTreeRouter.put('/:id')
-infectedTreeRouter.delete('/:id')
+infectedTreeRouter.get('/', InfectedTreeController.getAllUsersWrites)			// all user's writes
+infectedTreeRouter.get('/:id')			// one write
+infectedTreeRouter.post('/')			// create
+infectedTreeRouter.put('/:id')			// update
+infectedTreeRouter.delete('/:id')		// delete
 
 export default infectedTreeRouter
