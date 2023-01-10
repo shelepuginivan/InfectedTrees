9d1e36af0c51ebd0f9e60fd8fbab3ffd8950f528
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-10 08:40:40 +0300

    added user reference to InfectedTree model

--- server/src/models/InfectedTree.ts
+++ server/src/models/InfectedTree.ts
@@ -1,6 +1,7 @@
 import {Schema, model} from "mongoose";
 
 const InfectedTree = new Schema({
+	user: {type: Schema.Types.ObjectId, ref: "User"},
     lat: {type: Number, required: true},
     lon: {type: Number, required: true},
     photoURL: {type: String, required: true}
