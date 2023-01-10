3d92def3c1fc3a69ec614266b145ff0b76247458
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-10 21:19:11 +0300

    added interface for UserService

---
+++ server/src/interfaces/IUserService.ts
@@ -0,0 +1,5 @@
+import UserDTO from "../dtos/UserDTO";
+
+export interface IUserService {
+	updateUserData(organization: string, birthdate: number, phoneNumber: number, accessToken: string): Promise<UserDTO>
+}
