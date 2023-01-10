4819b14ab21f24e771da80be7b0bc93aa8597a3d
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-10 08:38:58 +0300

    added GET /:id and POST / handlers

--- server/src/routers/infectedTreeRouter.ts
+++ server/src/routers/infectedTreeRouter.ts
@@ -3,9 +3,9 @@ import InfectedTreeController from "../controllers/InfectedTreeController";
 
 const infectedTreeRouter: Router = Router()
 
-infectedTreeRouter.get('/', InfectedTreeController.getAllUsersWrites)			// all user's writes
-infectedTreeRouter.get('/:id')			// one write
-infectedTreeRouter.post('/')			// create
+infectedTreeRouter.get('/', InfectedTreeController.getAllUsersRecords)			// all user's records
+infectedTreeRouter.get('/:id', InfectedTreeController.getOneRecord)			// one record
+infectedTreeRouter.post('/', InfectedTreeController.create)					// create
 infectedTreeRouter.put('/:id')			// update
 infectedTreeRouter.delete('/:id')		// delete
 
