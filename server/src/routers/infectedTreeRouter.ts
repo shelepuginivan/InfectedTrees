f2311862fe30bb478bc48cb5d2db73e296bece79
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-10 09:39:24 +0300

    added PUT /:id and DELETE /:id handlers

--- server/src/routers/infectedTreeRouter.ts
+++ server/src/routers/infectedTreeRouter.ts
@@ -6,7 +6,7 @@ const infectedTreeRouter: Router = Router()
 infectedTreeRouter.get('/', InfectedTreeController.getAllUsersRecords)			// all user's records
 infectedTreeRouter.get('/:id', InfectedTreeController.getOneRecord)			// one record
 infectedTreeRouter.post('/', InfectedTreeController.create)					// create
-infectedTreeRouter.put('/:id')			// update
-infectedTreeRouter.delete('/:id')		// delete
+infectedTreeRouter.put('/:id', InfectedTreeController.update)					// update
+infectedTreeRouter.delete('/:id', InfectedTreeController.delete)				// delete
 
 export default infectedTreeRouter
