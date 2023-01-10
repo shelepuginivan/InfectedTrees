2cd8aac143c45aff7a1efb30d3cfc1baf86b9248
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-10 08:38:21 +0300

    implemented create method

--- server/src/services/InfectedTreeService.ts
+++ server/src/services/InfectedTreeService.ts
@@ -3,6 +3,7 @@ import InfectedTree from "../models/InfectedTree";
 import InfectedTreeDTO from "../dtos/InfectedTreeDTO";
 import {IInfectedTreeCreationData} from "../interfaces/IInfectedTreeCreationData";
 import TokenService from "./TokenService";
+import FileService from "./FileService"
 
 class InfectedTreeService {
 	async getAllUsersWrites(accessToken?: string): Promise<InfectedTreeDTO[]> {
@@ -18,15 +19,30 @@ class InfectedTreeService {
 	}
 
 	async getOneWriting(id: string): Promise<InfectedTreeDTO> {
+		if (!id) {
+			throw ServerException.BadRequest('parameter id required')
+		}
 		const writing = await InfectedTree.findById(id)
 		return new InfectedTreeDTO(writing)
 	}
 
-	async create(accessToken: string, creationData: IInfectedTreeCreationData) {
-		const userData = TokenService.validateAccessToken(accessToken)
+	async create(creationData: IInfectedTreeCreationData, accessToken?: string): Promise<InfectedTreeDTO> {
 		if (!accessToken) {
+			throw ServerException.Unauthorized('no access token provided')
+		}
+		const userData = TokenService.validateAccessToken(accessToken)
+		if (!userData) {
 			throw ServerException.Expired()
 		}
+		const filename = await FileService.writeFile(creationData.photo)
+		const infectedTreeData = {
+			user: userData.id,
+			lat: creationData.lat,
+			lon: creationData.lon,
+			photoURL: filename
+		}
+		const newRecord = await InfectedTree.create(infectedTreeData)
+		return new InfectedTreeDTO(newRecord)
 	}
 }
 
