8a0899290e8bd1856c9b66b12148e629dad89ddd
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:28:57 +0300

    imported styles

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
-import App from './App';
+import './index.css'
+import './css/variables.css'
+import App from './App'
+import '../src/fonts/icomoon/style.css'
 
 render(() => <App />, document.getElementById('root') as HTMLElement);
