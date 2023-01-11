6bd90083f277f72078d69243d4689801eeae1bc2
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:29:13 +0300

    added ui component Logo

---
+++ client/src/components/ui/Logo.tsx
@@ -0,0 +1,15 @@
+import {JSX} from "solid-js";
+import {HOME_ROUTE} from "../../utils/consts";
+import {A} from "@solidjs/router";
+
+const Logo = (): JSX.Element => {
+	return (
+		<A href={HOME_ROUTE}>
+			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80">
+				<image href="/src/assets/logo.svg"></image>
+			</svg>
+		</A>
+	);
+};
+
+export default Logo;
