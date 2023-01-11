5d6b0b7dda43fb66de4eda1fdfa0de54caec1b10
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:26:18 +0300

    added axios instance for authorized users

---
+++ client/src/utils/axiosInstanceAuthorized.ts
@@ -0,0 +1,8 @@
+import axios, {AxiosInstance} from "axios";
+
+export const axiosInstanceAuthorized = (accessToken: string): AxiosInstance => axios.create({
+	withCredentials: true,
+	headers: {
+		'Authorization': `Bearer ${accessToken}`
+	}
+})
