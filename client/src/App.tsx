608591df1184214442c805c4aa4cb96907b851d7
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:31:45 +0300

    added AppRouter and removed initial JSX

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,10 @@
-import type { Component } from 'solid-js';
+import {JSX} from "solid-js";
+import AppRouter from "./components/AppRouter";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
-
-const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
-};
+const App = (): JSX.Element => {
+	return (
+		<AppRouter/>
+	)
+}
 
 export default App;
