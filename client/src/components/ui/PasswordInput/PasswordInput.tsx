83dad85ccb4ea93f8deed2bb613c3d7764ac3496
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:26:52 +0300

    added ui component PasswordInput

---
+++ client/src/components/ui/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,15 @@
+import {createSignal, JSX} from 'solid-js'
+import styles from './passwordInput.module.css'
+
+const PasswordInput = (props: JSX.InputHTMLAttributes<any>): JSX.Element => {
+	const [visible, setVisible] = createSignal<boolean>(false)
+
+	return (
+		<div class={styles.inputWrapper}>
+			<input class={styles.input} placeholder={props.placeholder} value={props.value} onchange={props.onchange} type={visible() ? "text" : "password"} />
+			<button class={styles.watchButton} type="button" onClick={() => setVisible(prev => !prev)}><span class={visible() ? "icon-invisible" : "icon-visible"}></span></button>
+		</div>
+	)
+}
+
+export default PasswordInput
