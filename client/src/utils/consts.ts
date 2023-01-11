5553ebf6300b0bc5085a2fef73d1dbc535ba88fc
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:27:46 +0300

    added file with constant variables

---
+++ client/src/utils/consts.ts
@@ -0,0 +1,7 @@
+export const SERVER_HOST = 'http://localhost:8000'
+
+// Routes
+
+export const HOME_ROUTE = '/'
+export const LOGIN_ROUTE = '/auth/login'
+export const REGISTRATION_ROUTE = '/auth/registration'
