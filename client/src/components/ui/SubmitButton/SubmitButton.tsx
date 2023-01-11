b1e5be70560ed60012ac195f7295a09280445115
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-11 17:24:36 +0300

    added ui component SubmitButton

---
+++ client/src/components/ui/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,9 @@
+import {JSX} from 'solid-js'
+import styles from './submitButton.module.css'
+const SubmitButton = (props: JSX.ButtonHTMLAttributes<any>): JSX.Element => {
+	return (
+		<button class={styles.submitButton} onclick={props.onclick} type="button">{props.children}</button>
+	)
+}
+
+export default SubmitButton
