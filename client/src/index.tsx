1393250d3cd368bcec69495d38a61196d9549ab4
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-12 19:09:59 +0300

    added common styles import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web';
 
 import './index.css'
 import './css/variables.css'
+import './css/common.css'
 import App from './App'
 import '../src/fonts/icomoon/style.css'
 
