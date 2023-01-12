24e51a2d905dfbe53961649d4fd63944e013a797
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-12 19:09:01 +0300

    added home route

--- client/src/utils/routes.ts
+++ client/src/utils/routes.ts
@@ -1,9 +1,14 @@
 import {RouteDependency} from "./types/RouteDependency";
-import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "./consts";
+import {HOME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE} from "./consts";
 import LoginPage from "../pages/LoginPage";
 import RegistrationPage from "../pages/RegistrationPage";
+import HomePage from "../pages/HomePage";
 
 export const unauthorizedRoutes: RouteDependency[] = [
+	{
+		route: HOME_ROUTE,
+		component: HomePage
+	},
 	{
 		route: LOGIN_ROUTE,
 		component: LoginPage
