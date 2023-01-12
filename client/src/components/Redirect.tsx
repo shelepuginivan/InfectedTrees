13fea2607ea218a97162e3c3040f7b41d7fdf2dd
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-12 19:09:40 +0300

    implemented component for redirect

---
+++ client/src/components/Redirect.tsx
@@ -0,0 +1,13 @@
+import {useNavigate} from '@solidjs/router'
+
+type navigatorProps = {
+	to: string
+	replace: boolean
+}
+
+const Redirect = (props: navigatorProps): any => {
+	const navigate = useNavigate()
+	return navigate(props.to, {replace: props.replace})
+}
+
+export default Redirect
