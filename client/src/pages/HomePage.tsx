8d6628e0db8bf0e03b99e9ddbbc1c2b93aabd7b0
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-13 02:43:03 +0300

    added home page

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import {JSX} from "solid-js";
+import Header from "../components/Header";
+
+const HomePage = (): JSX.Element => {
+	return (
+		<div class="page">
+			<Header/>
+		</div>
+	);
+};
+
+export default HomePage;
