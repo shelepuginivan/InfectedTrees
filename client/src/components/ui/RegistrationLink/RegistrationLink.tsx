dccfd29977adcd327cf22b880baeea894ccd5b7d
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-12 19:08:46 +0300

    added ui component RegistrationLink

---
+++ client/src/components/ui/RegistrationLink/RegistrationLink.tsx
@@ -0,0 +1,12 @@
+import {JSX} from 'solid-js'
+import {A} from '@solidjs/router'
+import {LOGIN_ROUTE} from '../../../utils/consts'
+import styles from './registrationLink.module.css'
+
+const RegistrationLink = (): JSX.Element => {
+	return (
+		<A class={styles.registrationLink} href={LOGIN_ROUTE}>Зарегистрироваться</A>
+	)
+}
+
+export default RegistrationLink
