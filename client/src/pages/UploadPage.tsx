a36f62cd021aed625cc0e165e2d357830158302d
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-13 02:37:24 +0300

    added more common styles

---
+++ client/src/pages/UploadPage.tsx
@@ -0,0 +1,11 @@
+import PhotoUploadForm from "../components/PhotoUploadForm";
+
+const UploadPage = () => {
+	return (
+		<div class="page">
+			<PhotoUploadForm/>
+		</div>
+	)
+}
+
+export default UploadPage;
