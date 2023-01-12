f523ef17575552baba3ee6212978dbe01f810032
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-13 02:41:05 +0300

    added type for FileUploadArea props

---
+++ client/src/utils/types/FileUploadAreaProps.ts
@@ -0,0 +1,12 @@
+import {JSX} from "solid-js";
+
+export type FileUploadAreaProps = {
+	ondragstart: (e: DragEvent) => void
+	ondragleave: (e: DragEvent) => void
+	ondrop: (e: DragEvent) => void
+	inputFile: (e: Event) => void
+	clearInput: () => void
+	imagePreviewURL?: string
+	anyImageUploaded: boolean
+	children?: JSX.Element
+}
