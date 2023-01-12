41e11b82c299394e53bb4b3cd3188340b09959b9
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-12 19:10:50 +0300

    added redirect if route doesn't match allowed routes

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -2,6 +2,8 @@ import {JSX} from "solid-js";
 import {Route, Router, Routes} from "@solidjs/router";
 import {unauthorizedRoutes} from "../utils/routes";
 import Header from "./Header";
+import Redirect from "./Redirect";
+import {HOME_ROUTE} from "../utils/consts";
 
 
 const AppRouter = (): JSX.Element => {
@@ -21,6 +23,7 @@ const AppRouter = (): JSX.Element => {
 			<Router>
 				<Routes>
 					{unauthorizedRoutes.map(item => <Route path={item.route} component={item.component}/>)}
+					<Route path='*' element={<Redirect to={HOME_ROUTE} replace={true}/>}/>
 				</Routes>
 			</Router>
 		)
