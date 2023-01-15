507fb4834c25c4575c1e4f69d933885667989012
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-15 20:42:53 +0300

    added uploadTime field to InfectedTree db model

--- server/src/models/InfectedTree.ts
+++ server/src/models/InfectedTree.ts
@@ -4,7 +4,8 @@ const InfectedTree = new Schema({
 	user: {type: Schema.Types.ObjectId, ref: "User"},
     lat: {type: Number, required: true},
     lon: {type: Number, required: true},
-    photoURL: {type: String, required: true}
+    photoURL: {type: String, required: true},
+	uploadTime: {type: Number, default: Date.now()}
 })
 
 export default model('InfectedTree', InfectedTree)
