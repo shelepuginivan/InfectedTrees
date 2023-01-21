b6d61afba63ea0b5b96cb577c19591fddb1e196f
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-21 22:34:46 +0300

    added getUserData method

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -19,6 +19,21 @@ class UserController {
 			}
 		}
 	}
+
+	async getUserData(req: Request, res: Response): Promise<void> {
+		try {
+			const accessToken = req.headers.authorization?.split(' ')[1]
+			const userData = await UserService.getUserData(accessToken)
+			res.status(200).json(userData)
+		} catch (e) {
+			if (e instanceof ServerException) {
+				res.status(e.status).json({message: e.message})
+			} else {
+				res.status(500).json({message: `Unexpected server error: ${(e as Error).message}`})
+				console.error(e)
+			}
+		}
+	}
 }
 
 export default new UserController()
