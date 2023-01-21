180cf2f57b8b452851944f0123eb7bef9334f133
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-21 22:34:10 +0300

    added endpoint for accessing user data

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -3,6 +3,7 @@ import UserController from "../controllers/UserController";
 
 const userRouter: Router = Router()
 
+userRouter.get('/', UserController.getUserData)
 userRouter.put('/', UserController.updateUserData)
 
 export default userRouter
