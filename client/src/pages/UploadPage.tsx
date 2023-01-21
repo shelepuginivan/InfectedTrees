74f1c273a63d822f5a41e4d2cf967606c9835102
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-21 22:34:58 +0300

    added h1 header

--- client/src/pages/UploadPage.tsx
+++ client/src/pages/UploadPage.tsx
@@ -3,6 +3,7 @@ import InfectedTreeUploadForm from "../components/InfectedTreeUploadForm";
 const UploadPage = () => {
 	return (
 		<div class="page">
+			<h1 class="main-page-header">Новая запись</h1>
 			<InfectedTreeUploadForm/>
 		</div>
 	)
