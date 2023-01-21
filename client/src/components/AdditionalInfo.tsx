9762e31b0b4efc67becfe3e80736ef602253c00f
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-21 22:47:42 +0300

    added AdditionalInfo component

---
+++ client/src/components/AdditionalInfo.tsx
@@ -0,0 +1,67 @@
+import {createSignal, JSX, onMount, Show} from "solid-js";
+import DateInput from "./ui/DateInput/DateInput";
+import TextInput from "./ui/TextInput/TextInput";
+import PhoneInput from "./ui/PhoneInput/PhoneInput";
+import SubmitButton from "./ui/SubmitButton/SubmitButton";
+import ActionButton from "./ui/ActionButton/ActionButton";
+import styles from '../css/additionalInfo.module.css'
+import {axiosInstanceAuthorized} from "../utils/axiosInstanceAuthorized";
+import {SERVER_HOST} from "../utils/consts";
+import {formatDateInput} from "../utils/formatDateInput";
+import {interceptor} from "../utils/interceptor";
+
+const AdditionalInfo = (): JSX.Element => {
+	const [getIsEditingMode, setIsEditingMode] = createSignal<boolean>(false)
+	const [getBirthdate, setBirthdate] = createSignal<string>('')
+	const [getOrganizationName, setOrganizationName] = createSignal<string>('')
+	const [getPhoneNumber, setPhoneNumber] = createSignal<string>('')
+
+	const getAdditionalInfo = async () => {
+		const userResponse = await axiosInstanceAuthorized(sessionStorage.getItem('accessToken') as string).get(`${SERVER_HOST}/users`)
+		const {phoneNumber, organization, birthdate} = userResponse.data
+		setBirthdate(birthdate ?? '')
+		setPhoneNumber(phoneNumber ?? '')
+		setOrganizationName(organization ?? '')
+	}
+
+	onMount(async () => await interceptor(getAdditionalInfo, sessionStorage.getItem('accessToken') as string))
+
+	const uploadAdditionalInfo = async (): Promise<void> => {
+		const additionalData = {
+			birthdate: getBirthdate(),
+			organization: getOrganizationName(),
+			phoneNumber: getPhoneNumber()
+		}
+		const updateResponse = await axiosInstanceAuthorized(sessionStorage.getItem('accessToken') as string).put(`${SERVER_HOST}/users`, additionalData)
+		const {organization, phoneNumber, birthdate} = updateResponse.data
+		setBirthdate(birthdate)
+		setOrganizationName(organization)
+		setPhoneNumber(phoneNumber)
+	}
+
+	const submit = async () => {
+		await interceptor(uploadAdditionalInfo, sessionStorage.getItem('accessToken') as string)
+		setIsEditingMode(false)
+	}
+
+
+	return (
+		<Show when={getIsEditingMode()} fallback={<div class={styles.additionalInfo}>
+			<h2>Дополнительная информация</h2>
+			<p class={styles.p}>Дата рождения: {getBirthdate() ?? 'не установлена'}</p>
+			<p class={styles.p}>Название организации: {getOrganizationName() ?? 'не установлено'}</p>
+			<p class={styles.p}>Телефон: {getPhoneNumber() ?? 'не установлен'}</p>
+			<ActionButton onclick={() => setIsEditingMode(true)}>Изменить</ActionButton>
+		</div>} keyed>
+			<div class={styles.additionalInfo}>
+				<h2>Дополнительная информация</h2>
+				<DateInput placeholder="День рождения" value={formatDateInput(getBirthdate())} onchange={e => setBirthdate((e.target as HTMLInputElement).value)}/>
+				<TextInput placeholder="Название организации" value={getOrganizationName()} onchange={e => setOrganizationName((e.target as HTMLInputElement).value)}/>
+				<PhoneInput placeholder="Телефон" value={getPhoneNumber()} onchange={e => setPhoneNumber((e.target as HTMLInputElement).value)}/>
+				<SubmitButton onclick={submit}>Сохранить изменения</SubmitButton>
+			</div>
+	</Show>
+	)
+}
+
+export default AdditionalInfo;
