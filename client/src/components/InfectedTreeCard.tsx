1181509724d77577242281e4b6472edf46e0cedd
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-21 22:45:42 +0300

    added InfectedTreeCard component

---
+++ client/src/components/InfectedTreeCard.tsx
@@ -0,0 +1,18 @@
+import {JSX} from "solid-js"
+import {InfectedTreeCardProps} from "../utils/types/InfectedTreeCardProps";
+import styles from '../css/infectedTreeCard.module.css'
+
+const InfectedTreeCard = (props: InfectedTreeCardProps): JSX.Element => {
+	return (
+		<div class={styles.card}>
+			<img class={styles.image} src={props.photoURL} alt=""/>
+			<div class={styles.data}>
+				<p>Широта: {props.lat}</p>
+				<p>Долгота: {props.lon}</p>
+				<p>Дата загрузки: {Intl.DateTimeFormat('ru').format(props.uploadTime)}</p>
+			</div>
+		</div>
+	)
+}
+
+export default InfectedTreeCard;
