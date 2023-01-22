6cd111c571fa691ad770a1c60a7d7f34f5c212e1
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 15:16:43 +0300

    added refresh token check to save user authorization

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,34 +1,54 @@
-import {JSX} from "solid-js";
+import {createSignal, JSX, onMount, Show} from "solid-js";
 import {Route, Router, Routes} from "@solidjs/router";
 import {authorizedRoutes, unauthorizedRoutes} from "../utils/routes";
 import Header from "./Header";
 import Redirect from "./Redirect";
-import {HOME_ROUTE} from "../utils/consts";
+import {HOME_ROUTE, SERVER_HOST} from "../utils/consts";
+import {axiosInstanceUnauthorized} from "../utils/axiosInstanceUnauthorized";
+import {AxiosError} from "axios";
 
 
 const AppRouter = (): JSX.Element => {
 	const authorized = Boolean(sessionStorage.getItem('accessToken'))
+	const [getAuthorized, setAuthorized] = createSignal<boolean>(authorized)
 
-	if (authorized) {
-		return (
-			<Router>
-				<Header/>
-				<Routes>
-					{authorizedRoutes.map(item => <Route path={item.route} component={item.component}/>)}
-					<Route path={'/'} element={<div></div>}/>
-				</Routes>
-			</Router>
-		)
-	} else {
-		return (
+	onMount(async () => {
+		try {
+			if (!getAuthorized()) {
+				const refreshResponse = await axiosInstanceUnauthorized.get(`${SERVER_HOST}/auth/refresh`)
+				const userData = refreshResponse.data
+				sessionStorage.setItem('accessToken', userData.accessToken)
+				sessionStorage.setItem('fullName', `${userData.user.firstname} ${userData.user.lastname}`)
+				sessionStorage.setItem('email', userData.user.email)
+				sessionStorage.setItem('hasAPIKey', userData.user.hasAPIKey.toString())
+				setAuthorized(true)
+			}
+		} catch (e) {
+			if (e instanceof AxiosError && e.status === 401) {
+				setAuthorized(false)
+			}
+		}
+
+	})
+
+	return (
+		<Show when={getAuthorized()} keyed={true} fallback={
 			<Router>
 				<Routes>
 					{unauthorizedRoutes.map(item => <Route path={item.route} component={item.component}/>)}
 					<Route path='*' element={<Redirect to={HOME_ROUTE} replace={true}/>}/>
 				</Routes>
 			</Router>
-		)
-	}
+		}>
+			<Router>
+				<Header/>
+					<Routes>
+						{authorizedRoutes.map(item => <Route path={item.route} component={item.component}/>)}
+						<Route path={'/'} element={<div></div>}/>
+					</Routes>
+			</Router>
+		</Show>
+	)
 }
 
 export default AppRouter
