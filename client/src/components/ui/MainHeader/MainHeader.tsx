3e15b385cd14dc5d18b5a855c1f9be8afc27cdc4
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 14:52:50 +0300

    added ui component MainHeader (instead of using common styles)

---
+++ client/src/components/ui/MainHeader/MainHeader.tsx
@@ -0,0 +1,10 @@
+import {JSX, ParentProps} from 'solid-js'
+import styles from './MainHeader.module.css'
+
+const MainHeader = (props: ParentProps): JSX.Element => {
+	return (
+		<h1 class={styles.mainHeader}>{props.children}</h1>
+	)
+}
+
+export default MainHeader
