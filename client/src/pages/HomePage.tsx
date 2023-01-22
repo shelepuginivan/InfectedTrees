214cf6b7aaea72e3a18a86b04d05b43325e6625c
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 15:17:34 +0300

    used ui components instead of common styles

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,11 +1,12 @@
 import {JSX} from "solid-js";
 import Header from "../components/Header";
+import Page from "../components/ui/Page/Page";
 
 const HomePage = (): JSX.Element => {
 	return (
-		<div class="page">
+		<Page>
 			<Header/>
-		</div>
+		</Page>
 	);
 };
 
