ca874ae27f384279f75622f240fd22e9689c30ce
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 14:52:39 +0300

    added ui component Page (instead of using common styles)

---
+++ client/src/components/ui/Page/Page.tsx
@@ -0,0 +1,10 @@
+import {JSX, ParentProps} from 'solid-js'
+import styles from './Page.module.css'
+
+const Page = (props: ParentProps): JSX.Element => {
+	return (
+		<div class={styles.page}>{props.children}</div>
+	)
+}
+
+export default Page
