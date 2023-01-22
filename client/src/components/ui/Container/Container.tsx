6ed6fe45fc4177097ecd5461309234cbac4b0ad1
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 14:53:15 +0300

    added ui component Container  (instead of using common styles)

---
+++ client/src/components/ui/Container/Container.tsx
@@ -0,0 +1,10 @@
+import {JSX, ParentProps} from 'solid-js'
+import styles from './Container.module.css'
+
+const Container = (props: ParentProps): JSX.Element => {
+	return (
+		<div class={styles.container}>{props.children}</div>
+	)
+}
+
+export default Container
