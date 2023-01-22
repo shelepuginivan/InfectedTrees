036f279a9b9f4ecc1e94afb1dcbadb6ae0707f03
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 14:33:19 +0300

    added ui component Code

---
+++ client/src/components/ui/Code/Code.tsx
@@ -0,0 +1,14 @@
+import styles from './code.module.css'
+
+const Code = (props: {children: string}) => {
+	return (
+		<code class={styles.code}>
+			<span>{props.children}</span>
+			<button class={styles.clipButton} onclick={() => navigator.clipboard.writeText(props.children)}>
+				<span class="icon-clip"></span>
+			</button>
+		</code>
+	)
+}
+
+export default Code
