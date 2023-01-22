27d9f221e312fb1f711615df99c9f626c4f1956c
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 15:21:19 +0300

    removed common.css file from imports

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import { render } from 'solid-js/web';
 
 import './index.css'
 import './css/variables.css'
-import './css/common.css'
 import App from './App'
 import '../src/fonts/icomoon/style.css'
 
