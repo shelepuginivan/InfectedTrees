557397cf1b983b1d23a2ad0c7166afdcb26ed4fe
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-22 15:10:38 +0300

    fixed css module invalid class name

--- client/src/components/ui/TwoColumns/TwoColumns.tsx
+++ client/src/components/ui/TwoColumns/TwoColumns.tsx
@@ -3,7 +3,7 @@ import styles from './TwoColumns.module.css'
 
 const TwoColumns = (props: ParentProps): JSX.Element => {
 	return (
-		<div class={styles.twoCols}>{props.children}</div>
+		<div class={styles.twoColumns}>{props.children}</div>
 	)
 }
 
