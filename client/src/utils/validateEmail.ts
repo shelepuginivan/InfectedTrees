d44e425571a5af5b996489d7b530c95fc69a2c46
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-29 00:19:31 +0300

    created email validation function

---
+++ client/src/utils/validateEmail.ts
@@ -0,0 +1,4 @@
+export const validateEmail = (email: string): boolean => {
+	const emailRegexp = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+	return emailRegexp.test(email);
+}
