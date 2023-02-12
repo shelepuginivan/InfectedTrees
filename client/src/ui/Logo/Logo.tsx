223c12209b1334f014c2c0dcf6925e46eff4ec49
shelepuginivan <shelepuginivanm@gmail.com>
2023-02-12 17:23:17 +0300

    modified logo

--- client/src/ui/Logo/Logo.tsx
+++ client/src/ui/Logo/Logo.tsx
@@ -1,13 +1,15 @@
 import {JSX} from "solid-js";
-import {HOME_ROUTE} from "../utils/consts";
+import {HOME_ROUTE} from "../../utils/consts";
 import {A} from "@solidjs/router";
+import styles from './logo.module.css'
 
 const Logo = (): JSX.Element => {
 	return (
-		<A href={HOME_ROUTE}>
+		<A href={HOME_ROUTE} class={styles.logo}>
 			<svg xmlns="http://www.w3.org/2000/svg" width="80" height="80">
 				<image href="/src/assets/logo.svg"></image>
 			</svg>
+			<h1>Infected<br/>Trees</h1>
 		</A>
 	);
 };
