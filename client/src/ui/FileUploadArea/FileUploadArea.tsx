141589a60a245563ebb56ac2f13b7cd296fbb2d5
shelepuginivan <shelepuginivanm@gmail.com>
2023-06-07 17:08:52 +0300

    refactor: declare props type inside component file and refactor this type

--- client/src/ui/FileUploadArea/FileUploadArea.tsx
+++ client/src/ui/FileUploadArea/FileUploadArea.tsx
@@ -1,8 +1,15 @@
 import {JSX} from 'solid-js'
-import {FileUploadAreaProps} from '../../utils/types/FileUploadAreaProps'
 import styles from './fileUploadArea.module.css'
 
-const FileUploadArea = (props: FileUploadAreaProps): JSX.Element => {
+type PropsType = {
+	clearInput: () => void
+	imagePreviewURL?: string
+	anyImageUploaded: boolean
+	uploadFailed: boolean
+} & Pick<JSX.InputHTMLAttributes<HTMLInputElement>, 'ondragleave' | 'onchange'> &
+	Pick<JSX.ObjectHTMLAttributes<HTMLDivElement>, 'ondragstart' | 'ondrop'>
+
+const FileUploadArea = (props: PropsType): JSX.Element => {
 	return (
 		<div class={styles.container}>
 			<div class={styles.preview} data-visible={Boolean(props.imagePreviewURL).toString()} >
@@ -16,8 +23,8 @@ const FileUploadArea = (props: FileUploadAreaProps): JSX.Element => {
 				data-uploaded={props.anyImageUploaded.toString()}
 				data-upload-failed={props.uploadFailed}
 				ondragover={e => e.preventDefault()}
-			>{props.children} или&nbsp;<label class={styles.label} for="infectedTreePhotoInput">выберите файл</label>
-				<input id="infectedTreePhotoInput" class={styles.input} type="file" datatype="image/*" onchange={props.inputFile}/></div>
+			>Перетащите фото или&nbsp;<label class={styles.label} for="infectedTreePhotoInput">выберите файл</label>
+				<input id="infectedTreePhotoInput" class={styles.input} type="file" datatype="image/*" onchange={props.onchange}/></div>
 		</div>
 	)
 }
